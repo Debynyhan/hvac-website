bd3dfa71a2db14da521e85089cfdb8f1658980ce
debynyhan <debynyhan.banks@snhu.edu>
2025-04-01 12:39:52 -0400

    Initial project setup with Spring Initializr and dependencies

---
+++ src/main/java/com/clevelandaffordablehvac/hvac_website/HvacWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.clevelandaffordablehvac.hvac_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HvacWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HvacWebsiteApplication.class, args);
+	}
+
+}
